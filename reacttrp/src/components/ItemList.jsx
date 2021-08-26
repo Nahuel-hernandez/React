b1c012894a51549f78b7709d4d5442d37c976e78
Nahuel-hernandez <82951597+Nahuel-hernandez@users.noreply.github.com>
2021-08-26 18:57:27 -0300

    Add files via upload

---
+++ reacttrp/src/components/ItemList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Item from './Item'
+
+<Item />
+
+let styles= {
+
+    maxWidth: 250,
+    marginLeft:800
+}
+
+
+function ItemList({Item}) {
+    return (
+        <ul className="card text-danger border-danger mb-3 " style={styles}>
+             <div className="app" >
+        
+        { Item.map((juego)=> <div key={juego.id} className="card border-danger">
+            <div className="card-body text-danger ">
+                <div className="card-title "> {juego.nombre} </div>
+                </div>       
+                <div>
+                    <div className="card-text text-danger">Precio: {juego.precio} AR$</div> <br />
+                    <div className="card-text text-danger">Genero: {juego.genero}</div> 
+                    </div>
+                    </div>)} 
+        
+    </div>
+        </ul>
+    )
+}
+
+export default ItemList

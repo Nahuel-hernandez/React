1f58858521215aebd3355971aea0e935765007f8
Nahuel-hernandez <82951597+Nahuel-hernandez@users.noreply.github.com>
2021-09-02 02:17:10 -0300

    Add files via upload

--- reacttrp/src/App.js
+++ reacttrp/src/App.js
@@ -2,21 +2,34 @@ import './App.css';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-      <NavBar />
-      <br />
-      <ItemDetailContainer/>
-      <ItemListContainer />
-      </header>
-    </div>
     
+    <BrowserRouter>
+    <div className="App">
+      
+      <NavBar />
+      <Switch>
+      <Route exact path='/'>
+      <ItemListContainer />
+      </Route>
+
+      <Route exact path='/genero/:gener'>
+        <ItemListContainer />
+      </Route>
+
+      <Route exact path='/detalle'>
+      <ItemDetailContainer/>
+      </Route>
+      </Switch>
+      
+    </div>
+    </BrowserRouter>
   );
 }
-
-
 
 export default App;

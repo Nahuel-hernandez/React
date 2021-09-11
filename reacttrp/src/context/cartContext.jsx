02b2f876c9bf01444bde9b951bba8fce200b3654
Nahuel-hernandez <82951597+Nahuel-hernandez@users.noreply.github.com>
2021-09-11 00:58:10 -0300

    Add files via upload

--- reacttrp/src/context/cartContext.jsx
+++ reacttrp/src/context/cartContext.jsx
@@ -4,18 +4,36 @@ const CartContext = createContext()
 
 export const  useCartContext = () => useContext(CartContext) 
 
-function CartContextProvider({children}) {
-const [producto, setProducto] = useState ([{}])
 
-function agregarAlCarrito(prod){
-    setProducto([...producto,prod])
+
+function CartContextProvider({children}) {
+const [producto, setProducto] = useState ([])
+
+const agregarAlCarrito =(prod, cant)=>{
+    if (isInCart(prod.id)){
+        const updateCart = [...producto];
+
+        updateCart.forEach((element) => {
+            if(element.prod.id === prod.id) {
+                element.cant += cant
+            }
+        })
+        setProducto(updateCart)
+    }
+    else{
+    setProducto([...producto, {item: prod, quantity: cant}])
+}
+}
+
+const isInCart = (id)=>{
+    return producto.find((item) => item.id === parseInt (id))
 }
 
 function borrarCarrito (){
     setProducto([])
 }
 
-    console.log(producto)
+    console.log("test",producto ,)
 
     return (
         <CartContext.Provider value={{

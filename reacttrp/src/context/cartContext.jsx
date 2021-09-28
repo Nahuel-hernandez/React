54fdc8368beb724ba2ce49e465a0e459bc6e51e0
Nahuel-hernandez <82951597+Nahuel-hernandez@users.noreply.github.com>
2021-09-28 16:47:09 -0300

    Add files via upload

--- reacttrp/src/context/cartContext.jsx
+++ reacttrp/src/context/cartContext.jsx
@@ -1,33 +1,31 @@
 import { useContext, createContext ,  useState} from 'react'
 
-
 const CartContext = createContext()
 
 export const  useCartContext = () => useContext(CartContext) 
-
-
 function CartContextProvider({children}) {
+
 const [producto, setProducto] = useState ([])
 
 const agregarAlCarrito = (item, quantity)=>{
-	if(isInCart(item.id)) {
-        const updateCart = [...producto];
 
-        updateCart.forEach((element) => {
+if(isInCart(item.id)) {
+    const actualizarCart = [...producto];
+        actualizarCart.forEach((element) => {
             if(element.item.id === item.id) {
                 element.quantity += quantity
             }
         })
-        setProducto(updateCart)
+        setProducto(actualizarCart)
     } else {
         setProducto([...producto, {item, quantity}])
     }
 }
 
-console.log(producto)
-
 const isInCart = (id) => {
-    return producto.find(element => element.item.id === id)
+
+return producto.find(element => element.item.id === id)
+
 }
 
 const borrarCarrito =()=>{
@@ -36,20 +34,19 @@ const borrarCarrito =()=>{
 
 const borrarJuego = (itemId) =>{
     const filtro = producto.filter (element => element.item.id !== itemId)
-    return setProducto(filtro)
+
+return setProducto(filtro)
 
 }
 
-    return (
-        <CartContext.Provider value={{
-            producto,
-            agregarAlCarrito,borrarCarrito,borrarJuego
-
-        }}>
-
-            {children}
-        </CartContext.Provider>
-    )
+return (
+    <CartContext.Provider value={{
+        producto,
+        agregarAlCarrito,borrarCarrito,borrarJuego
+    }}>
+    {children}
+    </CartContext.Provider>
+)
 }
 
 export default CartContextProvider
